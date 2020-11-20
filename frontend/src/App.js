d4065f1ca051a231cfcbe1a13dc5212f8ad2f4c0
Jeonghyeon Kim <powergee101@gmail.com>
2020-11-20 13:47:26 +0900

    Add supports of SASS and Material UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="debug-root">
+      <Paper>
+        <h1>SCSS와 Material UI가 잘 설치되었는지 테스트합니다.</h1>
+        <h2>이 글이 붉은 색이고, 위 글이 남색이라면 성공입니다.</h2>
+      </Paper>
     </div>
   );
 }
