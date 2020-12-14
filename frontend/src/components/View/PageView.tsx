0a6916c71332bd77d6d67826e2dbf0472cd6db86
heeeun <01072417732w@gmail.com>
2020-12-14 09:53:09 +0900

    Make Button Component

--- frontend/src/components/View/PageView.tsx
+++ frontend/src/components/View/PageView.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import { Button } from '../UI';
 
 type PageViewProps = {
 	open : boolean;
@@ -17,6 +18,12 @@ function PageView({ open, handleSideBarOpen } : PageViewProps) {
 						: <ArrowForwardIosIcon onClick={handleSideBarOpen}>click</ArrowForwardIosIcon>
 				}
 				<h4>{open ? '열림' : '닫힘'}</h4>
+				<Button
+					classList={['hi', 'hello']} // 추가하고 싶은 class name 넘기기
+					value={<ArrowForwardIosIcon>hi</ArrowForwardIosIcon>} // 버튼 안에 표시하고 싶은 요소 넘기기
+					// tooltip="tooltip value" // tooltip으로 표시하고 싶은 string 넘기기
+					// ttside="right" // tooltip을 표시하고 싶은 위치 넘기기 (top, right, bottom, left)
+				/>
 			</Grid>
 		</>
 	);
