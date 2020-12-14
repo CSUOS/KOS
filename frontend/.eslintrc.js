5c5837ce46d87ed8a660d386a5dbb08ef5349a20
heeeun <01072417732w@gmail.com>
2020-12-14 09:50:16 +0900

    add eslint rule

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
 	'parser': '@typescript-eslint/parser',
-	'plugins': ["@typescript-eslint"],
+	'plugins': ['@typescript-eslint'],
 	'env': {
 		'commonjs': true,
 		'browser': true,
@@ -26,7 +26,7 @@ module.exports = {
 			'tab',
 		], // indent는 tab으로 통일
 		'react/jsx-indent' : [2, 'tab'], // jsx에서도 indent tab으로 통일
-		'react/jsx-filename-extension': [1, { "extensions": [".js", ".jsx", ".ts"] }], // 확장자 설정
+		'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx', '.ts'] }], // 확장자 설정
 		'linebreak-style': ['error', 'windows'], // CRLF
 		'no-unused-vars': 'warn', // 정의 후 사용하지 않은 변수는 경고만 하기
 		'no-use-before-define': 1, // 정의 전에 사용 금지
@@ -37,13 +37,15 @@ module.exports = {
 		'operator-linebreak': 0, // 연산자는 라인 앞쪽에 위치해야하는 rule
 		'comma-dangle': 0, // 마지막 요소에 ,를 붙여야하는 rule
 		'no-param-reassign': 0, // 파라미터는 지역변수로 받아서 쓰라는 rule
-    	'import/prefer-default-export': ["off"], // export const 문을 쓸때 에러를 내는 rule 해제
-		'react/prop-types': ["off"], // props의 타입체크를 처리에 proptypes가 아닌 typescript 사용 예정
+    	'import/prefer-default-export': ['off'], // export const 문을 쓸때 에러를 내는 rule 해제
+		'react/prop-types': ['off'], // props의 타입체크를 처리에 proptypes가 아닌 typescript 사용 예정
 		'react/jsx-wrap-multilines': 0, // jsx에서 여러 줄에 걸쳐서 정의할 때 복잡한 rule 해제
 		'react/jsx-indent-props': [2, 'tab'], // jsx의 속성에도 tab으로 indent 적용
 		'@typescript-eslint/explicit-module-boundary-types': 0, // function에 return 타입을 명시해야하는 rule 해제
 		'import/extensions': 0, // ts 파일을 불러오기 위해
 		'react/jsx-filename-extension': 0, // ts, tsx 파일에서도 jsx를 쓰기 위해
+		'no-restricted-syntax': ['warn', 'FunctionExpression', 'WithStatement', 'BinaryExpression'], // for in 사용
+		'react/jsx-props-no-spreading': ['warn'], // props로 받은 것 바로 props로 넘기기 허용
 	},
 	'settings': {
 		'react': {
@@ -51,7 +53,7 @@ module.exports = {
 		},
 		'import/resolver': {
 			'node': {
-				'extensions': [".js", ".jsx", ".ts", ".tsx"]
+				'extensions': ['.js', '.jsx', '.ts', '.tsx']
 			}
 		}
 	},
