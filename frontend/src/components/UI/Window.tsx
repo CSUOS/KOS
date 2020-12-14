81cadb8be69a15b7a0c48f979169af00a35dec80
leegwae <leegwae@gmail.com>
2020-12-15 04:17:42 +0900

    Add comment to Window and PageHeader

--- frontend/src/components/UI/Window.tsx
+++ frontend/src/components/UI/Window.tsx
@@ -3,7 +3,11 @@ import Backdrop from '@material-ui/core/Backdrop';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-
+/* ====[Window 사용 예시]========
+open: window가 열렸는지, 닫혔는지
+children: window 안에 들어갈 내용
+================================
+*/
 type WindowProps = {
 	open: boolean;
 	children: any;
