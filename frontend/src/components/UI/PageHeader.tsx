f5e8d78da757807a7fac370bc4843765dcdc0ffe
leegwae <leegwae@gmail.com>
2020-12-15 01:01:06 +0900

    Add page header component

---
+++ frontend/src/components/UI/PageHeader.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import CloseIcon from '@material-ui/icons/Close';
+import Button from './Button';
+
+type PageHeaderProps = {
+	mainTitle: string;
+	subTitle?: string | undefined;
+	hasCloseButton: boolean;
+	option: any;
+}
+
+function PageHeader(
+	{
+		mainTitle, subTitle, hasCloseButton, option
+	}: PageHeaderProps
+) {
+	return (
+		<Grid className="pageheader">
+			<Grid className="pageheader-main" container>
+				<h1>{mainTitle}</h1>
+				{hasCloseButton ? <Button
+					classList={['hi', 'hello']}
+					value={<CloseIcon />}
+				/> : undefined}
+			</Grid>
+			<Grid className="pageheader-main" container>
+				<Box className="pageheader-sub">{subTitle}</Box>
+				<Box className="pageheader-option">{option}</Box>
+			</Grid>
+		</Grid>
+	);
+}
+
+PageHeader.defaultProps = {
+	subTitle: undefined,
+};
+
+export default PageHeader;
