0c1af5b7936342394b82c97f1e5da2610e495db0
leegwae <leegwae@gmail.com>
2020-12-26 05:16:57 +0900

    Add date picker component

--- frontend/src/components/UI/index.tsx
+++ frontend/src/components/UI/index.tsx
@@ -4,3 +4,4 @@ export { default as Button } from './Button';
 export { default as PageHeader } from './PageHeader';
 export { default as Window } from './Window';
 export { default as AttributeValuePair } from './AttributeValuePair';
+export { default as DatePicker } from './DatePicker';
