c80a2266b726b6a77f619f45b102b99e2b52329d
Park Joonyoung <sparkjy18@gmail.com>
2020-12-26 18:19:23 +0900

    Added the description of AddBox component

--- frontend/src/components/UI/AddBox.tsx
+++ frontend/src/components/UI/AddBox.tsx
@@ -3,6 +3,7 @@ import Grid from '@material-ui/core/Grid';
 import AddIcon from '@material-ui/icons/Add';
 /* ====[AddBox 사용 예시]========
 mainTitle: AddBox에 표시할 설명
+비고: 리스트 내부에서 사용할 '태스크 추가하기', 리스트 외부에서 사용할 '리스트 추가하기' 버튼에 해당함
 ================================
 */
 
