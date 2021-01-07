15f8f9d3ecc5fe9982c526fd82a7ae64941146f5
heeeun <01072417732w@gmail.com>
2021-01-07 22:59:45 +0900

    add pid context provider

--- frontend/src/components/Provider.tsx
+++ frontend/src/components/Provider.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { OpenContextProvider, ProjectContextProvider } from './Model';
+import { OpenContextProvider, ProjectContextProvider, PIDContextProvider } from './Model';
 import ViewModel from './ViewModel';
 
 // Model과 View Model을 이어주는 역할
 const Provider = () => (
-	<ProjectContextProvider>
-		<OpenContextProvider>
-			<ViewModel />
-		</OpenContextProvider>
-	</ProjectContextProvider>
+	<PIDContextProvider>
+		<ProjectContextProvider>
+			<OpenContextProvider>
+				<ViewModel />
+			</OpenContextProvider>
+		</ProjectContextProvider>
+	</PIDContextProvider>
 );
 
 export default Provider;
