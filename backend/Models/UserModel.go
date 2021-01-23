4c4bf71424200179917073edb93c4ff1b885beeb
heeeun <01072417732w@gmail.com>
2021-01-23 20:17:41 +0900

    add icon to user model

--- backend/Models/UserModel.go
+++ backend/Models/UserModel.go
@@ -9,5 +9,6 @@ type User struct {
 	gorm.Model        // 모델에는 유니크 아이디랑 생성, 수정, 삭제 날짜가 기본적으로 포함.
 	Name       string // 유저의 아이디
 	Password   string // 유저의 비밀번호
+	icon       string // icon
 	GitID      string // GithubID
 }
