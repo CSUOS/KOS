dbb616ea8a872c031848cee4c223f400d07aade4
leegwae <leegwae@gmail.com>
2021-01-23 16:45:47 +0900

    minor update

--- frontend/src/components/Shared/AttributeSelect.tsx
+++ frontend/src/components/Shared/AttributeSelect.tsx
@@ -16,10 +16,11 @@ const Part = ({ subject, children }: PartProps) => (
 );
 
 const subjects = ['속성 이름 지정', '기본', '생성'];
-const defaultMenus = ['작성자', '멤버', '생성일자', '최근 수정일자', '데드라인', '진행 상태'];
+const defaultMenus = ['작성자', '최근 편집자', '멤버', '생성일자', '최근 수정일자', '데드라인', '진행 상태'];
 const createMenus = ['텍스트', '날짜', '단일 선택', '다중 선택', 'URL', '단일 체크박스'];
 const defaultPairs = [
 	{ type: 'creator', name: 'Creator', value: '김철수(kim)' },
+	{ type: 'editor', name: 'Editor', value: '김철수(kim)' },
 	{ type: 'member', name: 'Assign', value: { options: ['김철수(kim)', '우희은(hinge7)', '김정현(powergee)'], selectedOptions: ['김철수(kim)'] } },
 	{ type: 'createdAt', name: 'CreateAt', value: new Date() },
 	{ type: 'editedAt', name: 'EditedAt', value: new Date() },
