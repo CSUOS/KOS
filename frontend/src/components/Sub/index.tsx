4cc6c0f100cd023abe6c308c9ecfe1cce2713c85
Thereisnotruth <thereisnotruth12@gmail.com>
2021-01-23 18:55:21 +0900

    회원정보 설정 구현

--- frontend/src/components/Sub/index.tsx
+++ frontend/src/components/Sub/index.tsx
@@ -2,3 +2,4 @@ export { default as List } from './List';
 export { default as Task } from './Task';
 export { default as ProjectHead } from './ProjectHead';
 export { default as SideProject } from './SideProject';
+export { default as Setting } from './Setting';
