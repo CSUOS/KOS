ddd7f51f1e81c9c9fb4347acf884b6e959beb5f5
heeeun <01072417732w@gmail.com>
2021-01-25 16:14:32 +0900

    make task model use json data type

--- backend/Models/TaskModel.go
+++ backend/Models/TaskModel.go
@@ -2,6 +2,7 @@ package Models
 
 import (
 	"gorm.io/gorm"
+	"gorm.io/datatypes"
 )
 
 // Task 태스크에 관한 스키마
@@ -9,6 +10,21 @@ type Task struct {
 	gorm.Model // 모델에는 아이디랑 생성, 수정, 삭제 날짜가 기본적으로 포함.
 	Name       string
 	ListID     uint   // ListModel의 아이디를 가리킨다.
-	Attribute  string // 태스크 속성
-	Reactions  string // 리액션
+	Attribute  datatypes.JSON // 태스크 속성
+	/* 아래가 기본 틀
+	{
+        "type" : "타입",
+        "key" : "제목",
+        "value" : [
+            [true, "check1"],
+            [false, "check2"]
+        ]
+    }
+	*/
+	Reactions  datatypes.JSON // 리액션
+	/* 아래가 기본 틀
+	{
+		"emojiname" : ["user1", "user2"] 
+    }
+	*/
 }
