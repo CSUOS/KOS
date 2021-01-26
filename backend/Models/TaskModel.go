496d039f71aa8a8e7bab261de655f231cdf3cf89
heeeun <01072417732w@gmail.com>
2021-01-26 12:54:12 +0900

    modify task default datatype

--- backend/Models/TaskModel.go
+++ backend/Models/TaskModel.go
@@ -9,16 +9,18 @@ import (
 type Task struct {
 	gorm.Model // 모델에는 아이디랑 생성, 수정, 삭제 날짜가 기본적으로 포함.
 	Name       string
-	ListID     uint   // ListModel의 아이디를 가리킨다.
+	ListID     uint // ListModel의 아이디를 가리킨다.
+	Rank	   uint	// list 내의 index
 	Attribute  datatypes.JSON // 태스크 속성
 	/* 아래가 기본 틀
 	{
-        "type" : "타입",
-        "key" : "제목",
-        "value" : [
-            [true, "check1"],
-            [false, "check2"]
-        ]
+        "type" : [{
+			"key" : "제목",
+			"value" : [
+				[true, "check1"],
+				[false, "check2"]
+			]
+		}]
     }
 	*/
 	Reactions  datatypes.JSON // 리액션
