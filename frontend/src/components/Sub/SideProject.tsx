48a98567ded649944389194fbd618080340c7908
heeeun <gmldms784@naver.com>
2021-02-02 01:23:48 +0900

    set click to sidebar project

--- frontend/src/components/Sub/SideProject.tsx
+++ frontend/src/components/Sub/SideProject.tsx
@@ -1,9 +1,9 @@
-import React, { forwardRef, useState } from 'react';
+import React, { forwardRef, useState, Dispatch } from 'react';
 import clsx from 'clsx';
 
 import { Grid } from '@material-ui/core';
 
-import { useProjectState, ProjectObj } from '../Model';
+import { useProjectState, ProjectObj, usePIDDispatch } from '../Model';
 import { SubMenu, SideMenu } from '../Shared';
 
 type SideProjectProps = {
@@ -13,6 +13,7 @@ type SideProjectProps = {
 const SideProject = forwardRef<HTMLDivElement, SideProjectProps>(({ pid }, ref) => {
 	const [open, setOpen] = useState(false);
 	const project : ProjectObj | undefined = useProjectState();
+	const setPID : Dispatch<number> = usePIDDispatch();
 
 	return (
 		<Grid ref={ref} className="side-project">
@@ -21,7 +22,7 @@ const SideProject = forwardRef<HTMLDivElement, SideProjectProps>(({ pid }, ref) 
 				<Grid className="project-info">
 					<Grid className="info">
 						<Grid className={clsx('bg-color', project[pid].bgColor)}> </Grid>
-						<Grid className="project-name"><p>{project[pid].name.length > 14 ? `${project[pid].name.substr(0, 14)}...` : project[pid].name}</p></Grid>
+						<Grid className="project-name" onClick={() => setPID(pid)}><p>{project[pid].name.length > 14 ? `${project[pid].name.substr(0, 14)}...` : project[pid].name}</p></Grid>
 					</Grid>
 					<SideMenu
 						open={open}
