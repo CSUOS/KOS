f88cf0e4971ef3452c0b62bd113a7aef755be533
leegwae <leegwae@gmail.com>
2021-02-19 00:01:38 +0900

    Update AttributeValuePair.md

--- frontend/src/function/FunctionManager.tsx
+++ frontend/src/function/FunctionManager.tsx
@@ -37,5 +37,8 @@ export const getCurrentDate = () => {
 	return yyyy.concat('-') + mm.concat('-') + dd;
 };
 
-export const getRandomInt = (min: number, max:number) => (
-	Math.floor(Math.random() * (max - min + 1)) + min);
+export const getRandomInt = (min: number, max:number) => {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
+};
